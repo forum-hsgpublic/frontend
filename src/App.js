4523b6a72800ff4e182104e2fcdd96665ccc2532
hsgpublic <hsgpublic@gmail.com>
2024-11-03 22:38:50 +0900

    App에 각 컴포넌트 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Users from './components/Users';
+import Categories from './components/Categories';
+import Posts from './components/Posts';
+import Comments from './components/Comments';
+import Tags from './components/Tags';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/users">사용자 관리</Link></li>
+                        <li><Link to="/categories">카테고리 관리</Link></li>
+                        <li><Link to="/posts">게시물 관리</Link></li>
+                        <li><Link to="/comments">댓글 관리</Link></li>
+                        <li><Link to="/tags">태그 관리</Link></li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/categories" element={<Categories />} />
+                    <Route path="/posts" element={<Posts />} />
+                    <Route path="/comments" element={<Comments />} />
+                    <Route path="/tags" element={<Tags />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
